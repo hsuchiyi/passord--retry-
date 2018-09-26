d7e6ab02a4e35c1830d0d81f7d608feaff05fc1e
hsuchiyi <hsuchiyi@gmail.com>
2018-09-26 19:12:00 +0800

    version2

--- password.py
+++ password.py
@@ -1,9 +1,13 @@
-p = 0
-while p < 3:
+pwd = 'a123456'
+i = 3 
+while True:
     password = input('請輸入密碼(最多輸入三次): ')
-    if password != 'a123456':
-        print('密碼錯誤')
+    if password != pwd:
+        i = i - 1
+        print('密碼錯誤!還有', i, '次機會')
+        if i == 0:
+            break
     elif password == 'a123456':
         print('登入成功')
         break  
-    p = p + 1 
+
